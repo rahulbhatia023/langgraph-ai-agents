44e9b59d5512ac396a9c4461164b4b3638dd6e9e
Rahul Bhatia <rahul.bhatia023@gmail.com>
2024-10-01 13:47:11 -0400

    Preparing for deployment

--- AI_Agents.py
+++ AI_Agents.py
@@ -92,8 +92,6 @@ st.markdown(
 with st.sidebar:
     get_api_key("OPENAI_API_KEY")
     get_api_key("E2B_API_KEY")
-    get_api_key("FINANCIAL_DATASETS_API_KEY")
-    get_api_key("POLYGON_API_KEY")
 
 st.markdown("<h1>Welcome to the World of AI Agents</h1>", unsafe_allow_html=True)
 st.markdown("<h3>Where Intelligence Meets Innovation</h3>", unsafe_allow_html=True)
@@ -108,44 +106,6 @@ with col11:
         custom_card(
             title="Code Assistant",
             description="AI-powered assistant that integrates Python execution capabilities with React component rendering on the fly, offering a comprehensive environment for data analysis, visualization, and interactive web development.",
-        ),
-        unsafe_allow_html=True,
-    )
-
-with col12:
-    st.markdown(
-        custom_card(
-            "Financial Advisor",
-            "This financial agent integrates a stock market API to provide real-time stock data and detailed financial insights. Additionally, it incorporates the Tavily search API for broader web searches, offering a comprehensive tool for financial data and information retrieval.",
-        ),
-        unsafe_allow_html=True,
-    )
-
-with col13:
-    st.markdown(
-        custom_card(
-            "Reddit Search",
-            "This AI agent searches Reddit to find the most relevant answers to user queries across multiple subreddits. It intelligently analyzes responses, filters out the noise, and delivers the best possible answer by consolidating insights from various Reddit communities, providing users with accurate and curated information.",
-        ),
-        unsafe_allow_html=True,
-    )
-
-col21, col22, _ = container.columns(3)
-
-with col21:
-    st.markdown(
-        custom_card(
-            title="Research Analyst",
-            description="This agent customizes the research process by assembling AI analysts based on user-selected sources and topics. It conducts in-depth, multi-turn interviews with an expert AI to extract detailed insights, gathers information in parallel, and synthesizes the findings into a final report",
-        ),
-        unsafe_allow_html=True,
-    )
-
-with col22:
-    st.markdown(
-        custom_card(
-            "SQL Generator",
-            "This agent bridges the gap between natural language questions and dataset, allowing users to question about a dataset and receive insightful response. Users can ask questions about the data in natural language. The agent generates a SQL query based on the user's question, executes it on the dataset, and then displays the result and generate the human language response.",
         ),
         unsafe_allow_html=True,
     )
